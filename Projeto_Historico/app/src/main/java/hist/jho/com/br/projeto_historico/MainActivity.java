f9526e1dcc52143481c02325038d587dda64c519
JhoanesFreitas <jhoanesbarbosa@gmail.com>
2016-04-17 02:41:42 -0300

    Adicionado algumas cores

--- Projeto_Historico/app/src/main/java/hist/jho/com/br/projeto_historico/MainActivity.java
+++ Projeto_Historico/app/src/main/java/hist/jho/com/br/projeto_historico/MainActivity.java
@@ -39,99 +39,109 @@ import android.view.MenuItem;
 public class MainActivity extends AppCompatActivity
     implements NavigationView.OnNavigationItemSelectedListener{
 
- private CollapsingToolbarLayout collapsingToolbarLayout;
+  private CollapsingToolbarLayout collapsingToolbarLayout;
 
- @TargetApi(Build.VERSION_CODES.JELLY_BEAN) @Override
- protected void onCreate(Bundle savedInstanceState){
-  super.onCreate(savedInstanceState);
-  setContentView(R.layout.activity_main);
-  Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-  setSupportActionBar(toolbar);
+  @TargetApi(Build.VERSION_CODES.JELLY_BEAN) @Override
+  protected void onCreate(Bundle savedInstanceState){
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
 
-  FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-  fab.setOnClickListener(new View.OnClickListener(){
-   @Override
-   public void onClick(View view){
-    Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-        .setAction("Action", null).show();
-   }
-  });
+    FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+    fab.setOnClickListener(new View.OnClickListener(){
+      @Override
+      public void onClick(View view){
+        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+            .setAction("Action", null).show();
+      }
+    });
 
-  DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-  ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-      this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-  drawer.addDrawerListener(toggle);
-  toggle.syncState();
+    DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+    ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+        this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+    drawer.addDrawerListener(toggle);
+    toggle.syncState();
 
-  collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
-  collapsingToolbarLayout.setTitle("Iuiuiu");
-  Resources icon = getResources();
-  @SuppressLint({ "NewApi", "LocalSuppress" }) Drawable drawable = icon.getDrawable(R.drawable.ic_camara, getTheme());
-  collapsingToolbarLayout.setBackground(drawable);
-  if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-   collapsingToolbarLayout.setExpandedTitleColor(getResources().getColor(android.R.color.holo_green_dark, getTheme()));
+
+    collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
+    //collapsingToolbarLayout.setTitle("Iuiuiu");
+    collapsingToolbarLayout.setExpandedTitleColor(Color.CYAN);
+    collapsingToolbarLayout.setExpandedTitleTextAppearance(R.style.CollapsingToolbarExpanded);
+    Resources icon = getResources();
+    @SuppressLint({ "NewApi", "LocalSuppress" }) Drawable drawable = icon.getDrawable(R.drawable.ic_camara, getTheme());
+    collapsingToolbarLayout.setBackground(drawable);
+
+    if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+      collapsingToolbarLayout.setElevation(8);
+      toolbar.setElevation(8);
+    }
+
+
+  /*if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+   collapsingToolbarLayout.setExpandedTitleColor(getResources().getColor(android.R.color.transparent, getTheme()));
    collapsingToolbarLayout.setContentScrimResource(R.mipmap.ic_camara);
    collapsingToolbarLayout.setBackgroundResource(R.mipmap.ic_camara);
+  }*/
+
+    NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+    navigationView.setNavigationItemSelectedListener(this);
   }
 
-  NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-  navigationView.setNavigationItemSelectedListener(this);
- }
-
- @Override
- public void onBackPressed(){
-  DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-  if(drawer.isDrawerOpen(GravityCompat.START)){
-   drawer.closeDrawer(GravityCompat.START);
-  } else{
-   super.onBackPressed();
-  }
- }
-
- @Override
- public boolean onCreateOptionsMenu(Menu menu){
-  // Inflate the menu; this adds items to the action bar if it is present.
-  getMenuInflater().inflate(R.menu.main, menu);
-  return true;
- }
-
- @Override
- public boolean onOptionsItemSelected(MenuItem item){
-  // Handle action bar item clicks here. The action bar will
-  // automatically handle clicks on the Home/Up button, so long
-  // as you specify a parent activity in AndroidManifest.xml.
-  int id = item.getItemId();
-
-  //noinspection SimplifiableIfStatement
-  if(id == R.id.action_settings){
-   return true;
+  @Override
+  public void onBackPressed(){
+    DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+    if(drawer.isDrawerOpen(GravityCompat.START)){
+      drawer.closeDrawer(GravityCompat.START);
+    } else{
+      super.onBackPressed();
+    }
   }
 
-  return super.onOptionsItemSelected(item);
- }
-
- @SuppressWarnings("StatementWithEmptyBody")
- @Override
- public boolean onNavigationItemSelected(MenuItem item){
-  // Handle navigation view item clicks here.
-  int id = item.getItemId();
-
-  if(id == R.id.nav_camera){
-   // Handle the camera action
-  } else if(id == R.id.nav_gallery){
-
-  } else if(id == R.id.nav_slideshow){
-
-  } else if(id == R.id.nav_manage){
-
-  } else if(id == R.id.nav_share){
-
-  } else if(id == R.id.nav_send){
-
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu){
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.main, menu);
+    return true;
   }
 
-  DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-  drawer.closeDrawer(GravityCompat.START);
-  return true;
- }
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item){
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
+    int id = item.getItemId();
+
+    //noinspection SimplifiableIfStatement
+    if(id == R.id.action_settings){
+      return true;
+    }
+
+    return super.onOptionsItemSelected(item);
+  }
+
+  @SuppressWarnings("StatementWithEmptyBody")
+  @Override
+  public boolean onNavigationItemSelected(MenuItem item){
+    // Handle navigation view item clicks here.
+    int id = item.getItemId();
+
+    if(id == R.id.nav_camera){
+      // Handle the camera action
+    } else if(id == R.id.nav_gallery){
+
+    } else if(id == R.id.nav_slideshow){
+
+    } else if(id == R.id.nav_manage){
+
+    } else if(id == R.id.nav_share){
+
+    } else if(id == R.id.nav_send){
+
+    }
+
+    DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+    drawer.closeDrawer(GravityCompat.START);
+    return true;
+  }
 }
